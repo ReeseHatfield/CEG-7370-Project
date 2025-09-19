8ba4c7df2b669c8ab10aa9794ac7e3ea6f29a89f
ReeseHatfield <hatfield.69@wright.edu>
2025-09-19 18:06:08 -0400

    prototype http server
    still need to link public server with ollama stuff

--- phi-store/src/Main.java
+++ phi-store/src/Main.java
@@ -4,17 +4,23 @@ public class Main {
 
     public static void main(String[] args) throws Exception {
 
-        Ollama o = new OllamaBuilder()
-            .model(SupportedModels.LLAMA3DOT2)
-            .system("Recommend me products!")
-            .build();
+        // Ollama o = new OllamaBuilder()
+        //     .model(SupportedModels.LLAMA3DOT2)
+        //     .system("Recommend me products!")
+        //     .build();
 
-        try {
-            Response s = o.prompt("I usually like food products, please recommend me some");
+        // try {
+        //     Response s = o.prompt("I usually like food products, please recommend me some");
 
-            System.out.println(s.getFullResponse());
-        } catch (IOException ioe) {
-            ioe.printStackTrace();
-        }
+        //     System.out.println(s.getFullResponse());
+        // } catch (IOException ioe) {
+        //     ioe.printStackTrace();
+        // }
+
+        // todo document all these ports
+        PhiServer server = new PhiServer(3001);
+        server.listen();
+
+
     }
 }
--- phi-store/src/PhiServer.java
+++ phi-store/src/PhiServer.java
@@ -11,16 +11,49 @@ public class PhiServer {
 
     private final int portNum;
     private final HttpServer server;
-    private final static int SYSTEM_SOCKET_BACKLOG_DEFAULT = 0;
+    private static final int SYSTEM_SOCKET_BACKLOG_DEFAULT = 0;
 
     // todo make real exceptions
     public PhiServer(int port) throws Exception{
         this.portNum = port;
-        this.server = HttpServer.create(new InetSocketAddress(this.portNum), SYSTEM_SOCKET_BACKLOG_DEFAULT);
+        this.server = HttpServer.create(
+            new InetSocketAddress(this.portNum), 
+            SYSTEM_SOCKET_BACKLOG_DEFAULT
+        );
+
+        this.initEndpoints();
+        this.server.setExecutor(null);
 
     }
 
-    public void listen(){
 
+    public void listen(){
+        System.out.println("PhiSever listening on port " + this.portNum);
+
+        this.server.start();
+    }
+
+
+    private void initEndpoints(){
+        this.server.createContext("/test", new PhiServerHandler());
+    }
+
+    static class PhiServerHandler implements HttpHandler {
+
+        @Override
+        public void handle(HttpExchange exchange) throws IOException {
+            // exchange wraps both request and reponse
+            String wordsToSayBack = "Hello world";
+
+            // oracles javadoc claims bytes.length is better than just length, but it shouldnt matter
+            // for our usecase
+            exchange.sendResponseHeaders(200,  wordsToSayBack.getBytes().length);
+            OutputStream os = exchange.getResponseBody();
+            os.write(wordsToSayBack.getBytes());
+            os.close();
+
+        }
+    
+        
     }
 }
