cad9d416beb547cf1cffc03d25e86dcded68566e
ReeseHatfield <hatfield.69@wright.edu>
2025-10-21 07:49:32 -0400

    elastic IP

--- aws/ssh-via-dynamic-ip.sh
+++ aws/ssh-via-dynamic-ip.sh
@@ -4,11 +4,14 @@ KEY_PATH="$HOME/.ssh/dist-comp-aws.pem"
 
 USER="ec2-user"
 
-read -p "Public EC2 instance IP: " PUBLIC_IP
+# read -p "Public EC2 instance IP: " PUBLIC_IP
 
-if [ -z "$PUBLIC_IP" ]; then
-    echo "No IP entered. Exiting."
-    exit 1
-fi
+# if [ -z "$PUBLIC_IP" ]; then
+#     echo "No IP entered. Exiting."
+#     exit 1
+# fi
+
+# managed to snag an elastic IP
+PUBLIC_IP="52.6.125.198"
 
 ssh -i "$KEY_PATH" "$USER@$PUBLIC_IP"
